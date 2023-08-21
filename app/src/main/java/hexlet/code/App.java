a579ab9363476923cac97ece43dd5f1be2a197e5
kitdim <kitaykin9808@gmail.com>
2023-08-21 19:09:23 +0300

    init task3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+//    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+//    boolean help;
+//    @Option(names = {"-V", "--version"}, usageHelp = true, description = "Print version information and exit.")
+//    boolean version;
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
