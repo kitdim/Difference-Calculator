36d5878fdb307430f20752e647cd90837151a6fe
kitdim <kitaykin9808@gmail.com>
2023-08-21 23:20:27 +0300

    create Differ.java

---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+import java.util.Map;
+
+public class Differ {
+    public static String generate(Map<String, Object> data1, Map<String, Object> data2) {
+        return null;
+    }
+}
