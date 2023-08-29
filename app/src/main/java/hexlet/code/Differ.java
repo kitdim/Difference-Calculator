d25bcb750ae98f530ff0db7dfbcf1fcdcea7b06f
kitdim <kitaykin9808@gmail.com>
2023-08-29 22:29:33 +0300

    update Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,32 +1,29 @@
 package hexlet.code;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Differ {
-    public static String generate(Map<String, Object> data1, Map<String, Object> data2) {
-        StringBuilder result = new StringBuilder("{\n");
+    public static String generate(Map<String, Object> data1, Map<String, Object> data2, String format) {
+        List<String> differ = new LinkedList<>();
         Set<String> keys = new TreeSet<>(data1.keySet());
         keys.addAll(data2.keySet());
-
         for (var key : keys) {
             if (data1.containsKey(key)
-                    && data1.get(key).equals(data2.get(key))) {
-                result.append("    " + key + ": " + data1.get(key) + "\n");
+                    && Objects.equals(data1.get(key), data2.get(key))) {
+                differ.add("    " + key + ": " + data1.get(key));
             } else if (data1.containsKey(key)
                     && data2.containsKey(key)
-                    && !data1.get(key).equals(data2.get(key))) {
-                result.append("  - " + key + ": " + data1.get(key) + "\n");
-                result.append("  + " + key + ": " + data2.get(key) + "\n");
+                    && !Objects.equals(data1.get(key), data2.get(key))) {
+                differ.add("  - " + key + ": " + data1.get(key));
+                differ.add("  + " + key + ": " + data2.get(key));
             } else if (!data1.containsKey(key)
                     && data2.containsKey(key)) {
-                result.append("  + " + key + ": " + data2.get(key) + "\n");
+                differ.add("  + " + key + ": " + data2.get(key));
             } else {
-                result.append("  - " + key + ": " + data1.get(key) + "\n");
+                differ.add("  - " + key + ": " + data1.get(key));
             }
         }
-        result.append("}");
-        return result.toString();
+        Formatter formatter = new Formatter(differ, format);
+        return formatter.getFormat();
     }
 }
