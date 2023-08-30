c434ecc568b7872012306434bf8a1e0dc7eff781
kitdim <kitaykin9808@gmail.com>
2023-08-30 22:26:19 +0300

    update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,12 +12,9 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
-    //private Path filepath1 = Paths.get("/tmp/file.txt").toAbsolutePath();
     String filepath1;
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
-    //private Path filepath2 = Paths.get("/tmp/file.txt").toAbsolutePath();
     String filepath2;
-
     @Option(names = {"-f", "--format"}, defaultValue = "stylish", paramLabel = "format", description = "output format [default: stylish]")
     private String format;
 
