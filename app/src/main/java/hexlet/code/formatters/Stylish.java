e7da5311a1b06fa634a127912c3743b9012f58f8
kitdim <kitaykin9808@gmail.com>
2023-08-30 22:26:08 +0300

    add stylish formatter

---
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -0,0 +1,37 @@
+package hexlet.code.formatters;
+
+import java.util.List;
+
+public class Stylish {
+    public static String getFormatter(List<String> diff) {
+        StringBuilder result = new StringBuilder("{\n");
+        for (var item : diff) {
+            String[] items = item.split("\\|");
+            String optional = items[0];
+            switch (optional) {
+                case "+":
+                    result.append("  + ")
+                            .append(items[1])
+                            .append(": ")
+                            .append(items[2])
+                            .append("\n");
+                    break;
+                case "-":
+                    result.append("  - ")
+                            .append(items[1])
+                            .append(": ")
+                            .append(items[2])
+                            .append("\n");
+                    break;
+                case " ":
+                    result.append("    ")
+                            .append(items[1])
+                            .append(": ")
+                            .append(items[2])
+                            .append("\n");
+                    break;
+            }
+        }
+        return result.append("}").toString();
+    }
+}
