1e23f22e2f06dd2ca7e5f703d3dbbd50ad73cb19
kitdim <kitaykin9808@gmail.com>
2023-08-31 21:55:19 +0300

    update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,12 +11,15 @@ import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+    @Option(names = {"-f", "--format"},
+            defaultValue = "stylish",
+            paramLabel = "format",
+            description = "output format [default: stylish]")
+    private String format;
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     String filepath1;
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
     String filepath2;
-    @Option(names = {"-f", "--format"}, defaultValue = "stylish", paramLabel = "format", description = "output format [default: stylish]")
-    private String format;
 
     @Override
     public Integer call() throws IOException {
