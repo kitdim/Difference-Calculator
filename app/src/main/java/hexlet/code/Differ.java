2b9f2b28bbd4bfd47a6eaed25252df815395091a
kitdim <kitaykin9808@gmail.com>
2023-09-03 11:26:24 +0300

    refactoring

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 
-class Differ {
+public class Differ {
     public static List<String> generate(Map<String, Object> data1, Map<String, Object> data2) {
         List<String> differ = new LinkedList<>();
         Set<String> keys = new TreeSet<>(data1.keySet());
