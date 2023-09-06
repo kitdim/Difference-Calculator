f9e0ae5cd87031bd0ea8bab29cfcb308a5a4a628
kitdim <kitaykin9808@gmail.com>
2023-09-06 21:26:29 +0300

    fix https://github.com/kitdim/java-project-71/blob/2b9f2b28bbd4bfd47a6eaed25252df815395091a/app/src/main/java/hexlet/code/Parser.java#L29 https://github.com/kitdim/java-project-71/blob/2b9f2b28bbd4bfd47a6eaed25252df815395091a/app/src/main/java/hexlet/code/Parser.java#L14 И насчет парсера скажу, потому что тут тоже довольно сложный момент. Что такое парсер вообще. Может ты слышал про парсер html-страниц, страниц в интернете. Парсер - это такая программка, которой на вход скармливаешь строку, а она тебе возвращает данные из этой строки в виде какой-то структуры данных. Абстрактный пример: у меня есть какой-то документ, ты пишешь парсер, который вытаскивает из этого документа все предлоги и засовывает в List. Это самый простенький пример. Структуры могут быть сложные, иерархические. Но главное суть. А суть заключается в том, что парсер просто разбирает по полочкам какие-то данные в структуру и больше он не делает ничего. И так надо бы сделать и в нашей программе. Не стоит делать парсер ООП-сущностью с полями-путями, это не то. Давай просто в Differ.generate() приведем каждый из путей к абсолюту, прочитаем контент, выберем парсер (yaml или json), парсером получим структуру - все! Парсер будет по сути заниматься только этим https://github.com/kitdim/java-project-71/blob/2b9f2b28bbd4bfd47a6eaed25252df815395091a/app/src/main/java/hexlet/code/Parser.java#L29

--- app/src/main/java/hexlet/code/utils/Parser.java
+++ app/src/main/java/hexlet/code/utils/Parser.java
@@ -8,32 +8,18 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Parser {
-    private final Path filepath1;
-    private final Path filepath2;
-    private Map<String, Object> data1;
-    private Map<String, Object> data2;
-
-    Parser(String filepath1, String filepath2) {
-        this.filepath1 = Paths.get(filepath1).toAbsolutePath().normalize();
-        this.filepath2 = Paths.get(filepath2).toAbsolutePath().normalize();
-    }
-
-    public void parse() throws IOException {
-        ObjectMapper objectMapper = factoryMapper();
-        String file1 = Files.readString(this.filepath1);
-        String file2 = Files.readString(this.filepath2);
-        data1 = objectMapper.readValue(file1, new TypeReference<>() {
-        });
-        data2 = objectMapper.readValue(file2, new TypeReference<>() {
+    public static Map<String, Object> parse(Path filepath) throws IOException {
+        ObjectMapper objectMapper = factoryMapper(filepath);
+        String file = Files.readString(filepath);
+        return objectMapper.readValue(file, new TypeReference<>() {
         });
     }
 
-    private ObjectMapper factoryMapper() {
-        String type = String.valueOf(filepath1).split("\\.")[1];
+    private static ObjectMapper factoryMapper(Path path) {
+        String type = String.valueOf(path).split("\\.")[1];
         switch (type) {
             case "json" -> {
                 return new JsonMapper();
@@ -43,13 +29,5 @@ public class Parser {
             }
             default -> throw new RuntimeException();
         }
-    }
-
-    public Map<String, Object> getData1() {
-        return data1;
-    }
-
-    public Map<String, Object> getData2() {
-        return data2;
     }
 }
