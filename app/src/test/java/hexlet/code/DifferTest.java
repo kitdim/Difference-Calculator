39c88c0a605adcb8cd6f03dc29b5fa002a471652
kitdim <kitaykin9808@gmail.com>
2023-09-13 11:19:04 +0300

    fix https://github.com/kitdim/java-project-71/blob/6d75c4243af0ce8440be0d70f4cb896eead4725d/app/src/test/java/hexlet/code/DifferTest.java#L19-L97 Этих строк не должно быть не теле теста. Результаты лучше поместить в файлы и их оттуда подрузить в @BeforeAll

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -3,99 +3,36 @@ package hexlet.code;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DifferTest {
-    String expect = "";
-    String expectRec = "";
-    String expectPlain = "";
-    String expectJson = "";
-    String[] formatters;
+    static String expect = "";
+    static String expectRec = "";
+    static String expectPlain = "";
+    static String expectJson = "";
+    static String[] formatters;
 
-    @BeforeEach
-    public void initExpect() {
-        expect = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
-        expectRec = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  + obj1: {nestedKey=value, isNested=true}
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
-        expectPlain = """
-                Property 'chars2' was updated. From [complex value] to false
-                Property 'checked' was updated. From false to true
-                Property 'default' was updated. From null to [complex value]
-                Property 'id' was updated. From 45 to null
-                Property 'key1' was removed
-                Property 'key2' was added with value: 'value2'
-                Property 'numbers2' was updated. From [complex value] to [complex value]
-                Property 'numbers3' was removed
-                Property 'numbers4' was added with value: [complex value]
-                Property 'obj1' was added with value: [complex value]
-                Property 'setting1' was updated. From 'Some value' to 'Another value'
-                Property 'setting2' was updated. From 200 to 300
-                Property 'setting3' was updated. From true to 'none'
-                """;
-        expectJson = """
-                {
-                  "chars1": ["a", "b", "c"],
-                  "chars2": ["d", "e", "f"],
-                  "chars2": false,
-                  "checked": false,
-                  "checked": true,
-                  "default": null,
-                  "default": ["value1", "value2"],
-                  "id": 45,
-                  "id": null,
-                  "key1": "value1",
-                  "key2": "value2",
-                  "numbers1": [1, 2, 3, 4],
-                  "numbers2": [2, 3, 4, 5],
-                  "numbers2": [22, 33, 44, 55],
-                  "numbers3": [3, 4, 5],
-                  "numbers4": [4, 5, 6],
-                  "obj1": {
-                    "nestedKey": "value",
-                    "isNested": true
-                  },
-                  "setting1": "Some value",
-                  "setting1": "Another value",
-                  "setting2": 200,
-                  "setting2": 300,
-                  "setting3": true,
-                  "setting3": "none"
-                }""";
+    @BeforeAll
+    public static void initExpect() throws IOException {
+        Path stylishPath1 = Paths.get("src/test/resources/Expect/stylishFile1.txt");
+        Path stylishPath2 = Paths.get("src/test/resources/Expect/stylishFile2.txt");
+        Path plainPath = Paths.get("src/test/resources/Expect/plainFile1.txt");
+        Path jsonPath = Paths.get("src/test/resources/Expect/jsonFile1.txt");
+        expect = read(stylishPath1);
+        expectRec = read(stylishPath2);
+        expectPlain = read(plainPath);
+        expectJson = read(jsonPath);
         formatters = new String[]{"stylish", "plain", "json"};
+    }
+
+    public static String read(Path filepath) throws IOException {
+        return Files.readString(filepath);
     }
 
     @Test
